bdece2a4ddf9199359b24407f334a1ba17304f03
James Lee <jamestlee513@gmail.com>
2021-02-05 09:47:13 -0800

    Add first visit modal

--- react-app/src/components/SplashPage.js
+++ react-app/src/components/SplashPage.js
@@ -1,4 +1,4 @@
-import { Box, Fade, Flex, Image, LightMode, useColorMode } from '@chakra-ui/react';
+import { Box, Button, Flex, Image, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, useColorMode, useDisclosure } from '@chakra-ui/react';
 import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { Redirect } from 'react-router-dom';
@@ -10,11 +10,25 @@ function SplashPage() {
 
     const currentUser = useSelector(state => state.user);
     const { colorMode, toggleColorMode } = useColorMode();
+    const { isOpen, onOpen, onClose } = useDisclosure();
+
     useEffect(() => {
         if (colorMode === 'dark') {
             toggleColorMode()
         }
+
+        const hasVisited = localStorage.getItem('hasVisited');
+        if (!hasVisited) {
+            const interval = setTimeout(() => {
+                onOpen();
+                localStorage.setItem('hasVisited', true);
+            }, 2500);
+            return () => {
+                clearInterval(interval);
+            }
+        }
     }, [])
+
     if (currentUser.id) {
         return <Redirect to="/home" />
     }
@@ -69,17 +83,46 @@ function SplashPage() {
 
             </Box>
 
-                <Flex justify="space-around" align="center" w="90%">
-                    <FadeIn transitionDuration="2000">
-                        <Flex align="center">
-                            <Image h="200px" w="200px" src="https://i.ibb.co/RQ9wbWm/ergon-logo.png" />
-                            <Box fontSize="80px" color="white" fontFamily="Roboto Mono">Ergon</Box>
+            <Flex justify="space-around" align="center" w="90%">
+                <FadeIn transitionDuration="2000">
+                    <Flex align="center">
+                        <Image h="200px" w="200px" src="https://i.ibb.co/RQ9wbWm/ergon-logo.png" />
+                        <Box fontSize="80px" color="white" fontFamily="Roboto Mono">Ergon</Box>
+                    </Flex>
+                </FadeIn>
+                <FadeIn delay="1000" transitionDuration="2000">
+                    <LoginSigninPage />
+                </FadeIn>
+            </Flex>
+
+            <Modal isOpen={isOpen} onClose={onClose}>
+                <ModalOverlay />
+                <ModalContent>
+                    <ModalHeader>Welcome!</ModalHeader>
+                    <ModalCloseButton />
+                    <ModalBody>
+                        <Box p={2} m={2} mb={1}>
+                            Hi, thanks for stopping by! Ergon is a custom one-stop productivity app built to boost your work efficiency. This app was built with a React front-end and Flask back-end.
+                        </Box>
+                        <Flex mr={2} pr={2} pl={2} justify="flex-end">
+                            -James Lee
                         </Flex>
-                    </FadeIn>
-                    <FadeIn delay="1000" transitionDuration="2000">
-                        <LoginSigninPage />
-                    </FadeIn>
-                </Flex>
+                    </ModalBody>
+
+                    <ModalFooter>
+                        <Flex justify="space-between" align="center" w="100%" m={2} mt={0} p={1}>
+                            <Flex w="20%" justify="space-between">
+                                <a href="https://github.com/jamestlee513/ergon" target="_blank" rel="noopener noreferrer"><i className="fab fa-github"></i></a>
+                                <a href="https://www.linkedin.com/in/jameslee97/" target="_blank" rel="noopener noreferrer"><i className="fab fa-linkedin"></i></a>
+                                <a href="https://angel.co/u/james-lee-146" target="_blank" rel="noopener noreferrer"><i className="fab fa-angellist"></i></a>
+                            </Flex>
+                            <Button onClick={onClose}>
+                                Got it!
+                        </Button>
+                        </Flex>
+                    </ModalFooter>
+                </ModalContent>
+            </Modal>
         </>
     )
 }
